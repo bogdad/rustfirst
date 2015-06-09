b795e0e72863174a6bb5c20d9e4c7d06f5561a80
Vladimir Shakhov <vshakhov@yandex-team.ru>
2015-06-09 22:29:37 +0400

    simple http server

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,35 @@ use hyper::server::Request;
 use hyper::server::Response;
 use hyper::net::Fresh;
 
+#[derive(Debug)]
+enum NodeState {
+    Follower,
+    Candidate,
+    Leader
+}
+
+struct Node {
+    state: NodeState
+}
+
+impl Node {
+    pub fn new() -> Node {
+        Node{ state: NodeState::Follower }
+    } 
+}
+
 fn main() {
+    let node = Node::new();
     Server::http(hello).listen("127.0.0.1:3001").unwrap();
 }
 
-fn hello(_: Request, res: Response<Fresh>) {
+fn hello(req: Request, res: Response<Fresh>) {
     let mut res = res.start().unwrap();
-    res.write_all(b"Hello World!").unwrap();
+    let uri:String = format!("{:?}", req.uri);
+    match uri.find("/status") {
+        Some(_) => res.write_all(b"Status").unwrap(),
+        None => res.write_all(format!("Hello {:?}", uri).as_bytes()).unwrap()
+    }
     res.end().unwrap();
 }
+
